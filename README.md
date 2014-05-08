8380e573e67997e70093501ed2797d72e6719276
Tom (eli box) <tcorelis@retailinkjet.com>
2014-05-08 11:57:30 -0700

    sync with other code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bog
 
-TODO: Write a gem description
+Bog is a simple logging library for Ruby applications, which renders messages with colorization and caller information attached.
 
 ## Installation
 
@@ -18,7 +18,34 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Bog will attempt to create a `log/` in the root of your project. (It checks `Rails.root` if it's available, otherwise `Dir.pwd`).
+
+Bog manifests as a class of the same name. Various log channels are available as functions of `Bog`.
+
+There are:
+
+	Bog.debug("Yourtext")
+	Bog.info("Yourtext")
+	Bog.warn("Yourtext")
+	Bog.error("Yourtext")
+	Bog.fatal("Yourtext")
+	Bog.other("Yourtext")
+
+### Configuration
+
+`Bog.shut_up!` and `Bog.talk_again` will disable and enable logging, respectively.
+
+Log display levels can be modified, with a different setting for screen and disk output. Bog starts with all message types displayed in all categories, but can be configured with:
+
+	Bog.deny(type = :both, categories = [])
+	Bog.allow_only(type = :both, categories = [])
+
+`type` must be `:display`, `:log`, or `:both`.
+`categories` is an array of any of the following symbols: `:debug`, `:info`, `:warn`, `:error`, `:fatal`, `:other`
+
+Configuration can be reset with:
+	
+	Bog.reset
 
 ## Contributing
 
--- lib/bog.rb
+++ lib/bog.rb
@@ -1,8 +1,7 @@
-
 # Bog - an alternate logger
 # (C) 2012  Lyjia
 # http://www.github.com/lyjia
-#version 1.1
+# version 1.1
 
 class Bog
 
@@ -11,90 +10,154 @@ class Bog
 	else
 		@@log_dir = "#{Dir.pwd}/log"	
 	end
-	
-	Dir.mkdir(@@log_dir) unless File.exist?(@@log_dir)
-	@@log_filename = "#{@@log_dir}/bog.log"
-	
-	@@log = File.open(@@log_filename, 'a')
-	@@silenced = false
-	
-	LOG_TYPES = { debug: "[35m", 
-					info: "[34m", 
-					warn: "[35;1m", 
-					error: "[31m", 
-					fatal: "[31;1m", 
-					other: "[32m" }
-					
-	ESC_PREFIX = "\e"
-	COLOR_NORMAL = "[0m"
 
-	def self.format_message(severity, msg)
-		hello = self::get_my_caller()
-		["#{Time.now.strftime("%Y%m%d-%H%M%S")} #{severity} #{hello}():", "#{msg}"]
-	end
+  Dir.mkdir(@@log_dir) unless File.exist?(@@log_dir)
+  @@log_filename = "#{@@log_dir}/bog.log"
 
-	def self.shut_up!
-		bog.debug("Shutting up...")
-		@@silenced = true
-	end
+  @@log      = File.open(@@log_filename, 'a')
+  @@silenced = false
 
-	def self.talk_again!
-		@@silenced = false
-		bog.debug("I can talk again!")
-	end
-	
-	def self.method_missing(meth, *args, &block)
-	
-		#$stderr.puts "MM: Is meth '#{meth}' a valid word from #{LOG_TYPES.keys}? (#{LOG_TYPES.keys.include?(meth)})"
-		meth = meth.downcase.to_sym
-		
-		if LOG_TYPES.keys.include?(meth)
-			self::msg(meth, args[0])
-		else
-			super
-		end
-	
-	end
+  LOG_TYPES = { debug: "[35m",
+                info:  "[34m",
 
-	private
-	
-	def self.colorize(severity, msg)
-		
-		color = LOG_TYPES[severity.downcase.to_sym]
-		
-		col = "#{ESC_PREFIX}#{color}"
-		if [:error, :fatal].include?(severity)
-			white = "#{ESC_PREFIX}[37;1m"
-		else
-			white = "#{ESC_PREFIX}#{COLOR_NORMAL}"
-		end
-		reset = "#{ESC_PREFIX}#{COLOR_NORMAL}"
-		
-		return "#{col}#{msg[0]} #{white}#{msg[1]}#{reset}"
-	
-	end
-	
-	def self.msg(level, msg)
-		m = Bog::format_message(level, msg)
-		
-		displayer = :puts
-		writer = :bog
-		
-		unless @@silenced == true
-		
-			case displayer
-				when :puts
-					$stderr.puts Bog::colorize(level, m)
-			end
-			
-			case writer
-				when :bog
-					@@log.puts m.join(" ")					
-			end
-				
-		end
-		
-	end
+                warn:  "[33m",
+                error: "[31m",
+                fatal: "[31;1m",
+                other: "[32m" }
+
+  ESC_PREFIX   = "\e"
+  COLOR_NORMAL = "[0m"
+  TYPES        = [:both, :display, :log]
+
+  @@allowed_disp = LOG_TYPES.keys
+  @@allowed_log  = LOG_TYPES.keys
+
+
+  # User-facing functions
+  def self.allow_only(type = :both, cats = [])
+    raise ArgumentError, "Invalid type #{type}, must be one of these symbols: "<<TYPES.join(" ") unless TYPES.include?(type)
+
+    cats.each do |c|
+      raise ArgumentError, "Invalid category #{c}, must be one of these symbols: "<<LOG_TYPES.keys.join(" ") unless LOG_TYPES.keys.include?(c)
+    end
+
+    if type == :both || type == :display
+      @@allowed_disp = []
+      @@allowed_disp << cats
+      @@allowed_disp.flatten!
+    end
+
+    if type == :both || type == :log
+      @@allowed_log = []
+      @@allowed_log << cats
+      @@allowed_log.flatten!
+    end
+
+    self.info("Allowed categories changed. Display: #{@@allowed_disp}, log: #{@@allowed_log}")
+
+  end
+
+  def self.deny(type = :both, cats = [])
+    raise ArgumentError, "Invalid type, must be one of these symbols: "<<TYPES.join(" ") unless TYPES.include?(type)
+
+    cats.each do |c|
+      if type == :both || type == :display
+        @@allowed_disp.delete(c)
+      end
+
+      if type == :both || type == :log
+        @@allowed_log.delete(c)
+      end
+    end
+
+    self.info("Allowed categories changed. Display: #{@@allowed_disp}, log: #{@@allowed_log}")
+  end
+
+
+  def self.reset
+    @@allowed_disp = LOG_TYPES.keys
+    @@allowed_log  = LOG_TYPES.keys
+    self.info("Allowed categories changed. Display: #{@@allowed_disp}, log: #{@@allowed_log}")
+  end
+
+  def self.shut_up!
+    bog.debug("Shutting up...")
+    @@silenced = true
+  end
+
+
+  def self.talk_again!
+    @@silenced = false
+    bog.debug("I can talk again!")
+  end
+
+
+  # responds to Bog.info, Bog.error, etc
+  def self.method_missing(meth, *args, &block)
+
+    #$stderr.puts "MM: Is meth '#{meth}' a valid word from #{LOG_TYPES.keys}? (#{LOG_TYPES.keys.include?(meth)})"
+    meth = meth.downcase.to_sym
+
+    if LOG_TYPES.keys.include?(meth)
+      self::msg(meth, args[0])
+    else
+      super
+    end
+
+  end
+
+
+  private
+
+  # internal functions
+  def self.format_message(severity, msg)
+    hello = self::get_my_caller()
+    ["#{Time.now.strftime("%Y%m%d-%H%M%S")} #{severity} #{hello}():", "#{msg}"]
+  end
+
+
+  def self.colorize(severity, msg)
+
+    color = LOG_TYPES[severity.downcase.to_sym]
+
+    col = "#{ESC_PREFIX}#{color}"
+    if [:error, :fatal].include?(severity)
+      white = "#{ESC_PREFIX}[37;1m"
+    else
+      white = "#{ESC_PREFIX}#{COLOR_NORMAL}"
+    end
+    reset = "#{ESC_PREFIX}#{COLOR_NORMAL}"
+
+    return "#{col}#{msg[0]} #{white}#{msg[1]}#{reset}"
+
+  end
+
+
+  def self.msg(level, msg)
+    m = Bog::format_message(level, msg)
+
+    displayer = :puts
+    writer    = :bog
+
+    unless @@silenced == true
+
+      if @@allowed_disp.include?(level)
+        case displayer
+          when :puts
+            $stderr.puts Bog::colorize(level, m)
+        end
+      end
+
+      if @@allowed_log.include?(level)
+        case writer
+          when :bog
+            @@log.puts m.join(" ")
+        end
+      end
+
+    end
+
+  end
 
 	def self.get_my_caller()
 		#s = caller.grep(/\/app\//)[2]
@@ -104,5 +167,6 @@ class Bog
 		step = 3
 		caller(step+1)[0][/`.*'/][1..-2]
 	end
+
 
 end
